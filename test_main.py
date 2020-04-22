e4f2f0580dfe9b4d755f892e25d90e7870235692
Kodavali841 <56919318+Kodavali841@users.noreply.github.com>
2020-04-22 09:16:14 +0530

    removed spaces beside the =sign

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 import main
 def test_multiply():
-  result = main.multiply(3, 4)
+  result=main.multiply(3, 4)
   print('running tests...')
   assert result == 4 #throws an error if result != 12
    
