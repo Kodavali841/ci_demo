b7750f05ddd4c0a083165e541053ebb94b91e1a0
Kodavali841 <56919318+Kodavali841@users.noreply.github.com>
2020-04-23 17:54:48 +0530

    removed spacing between ==

--- test_main.py
+++ test_main.py
@@ -3,5 +3,5 @@ def test_guru() :
   result=main.multiply(3, 4)
   result_2=main.multiply(13, 4)
   print('running tests...')
-  assert result == 12 #throws an error if result != 12
-  assert result_2 == 13
+  assert result==12 #throws an error if result != 12
+  assert result_2==13
